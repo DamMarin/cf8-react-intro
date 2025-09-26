1e1d7e422d441d1820f4ad75c6300f80634bdb8a
dammarin <marinatosd@gmail.com>
2025-09-26 20:42:06 +0300

    feat: add ClassComponentWithState and FunctionalComponentWithState  components

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@
 // import FunctionalComponent from "./components/FunctionalComponent.tsx";
 // import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 // import Card from "./components/Card.tsx";
 import Layout from "./components/Layout.tsx";
+// import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";
+// import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
+import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 
 function App() {
   return (
@@ -26,12 +29,15 @@ function App() {
       {/*    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam."*/}
       {/*  />*/}
       {/*</Card>*/}
-      <Layout>
 
-          <ArrowFunctionalComponentWithPropsType
-            title="Is a Arrow Functional Component With 2 Props"
-            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam."
-          />
+      <Layout>
+          {/*<ArrowFunctionalComponentWithPropsType*/}
+          {/*  title="Is a Arrow Functional Component With 2 Props"*/}
+          {/*  description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam."*/}
+          {/*/>*/}
+
+        {/*<FunctionalComponentWithState />*/}
+        <ClassComponentWithState />
 
       </Layout>
     </>
